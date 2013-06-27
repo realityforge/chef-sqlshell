3bcd5cc5f141ee4446542e68b67206b1fe6a04e9
Peter Donald <peter@realityforge.org>
2013-06-28 09:59:30 +1000

    Ensure that logins for users are updated correctly if they change

--- providers/ms_user.rb
+++ providers/ms_user.rb
@@ -24,6 +24,28 @@ action :create do
     command "USE [#{new_resource.database}]; #{create_user_sql}"
     not_if_sql "SELECT * FROM [#{new_resource.database}].sys.sysusers WHERE name = '#{new_resource.user}'"
   end
+
+  update_user_sql = "ALTER USER [#{new_resource.user}] WITH LOGIN [#{new_resource.login}]"
+  sqlshell_exec update_user_sql do
+    jdbc_url new_resource.jdbc_url
+    jdbc_driver new_resource.jdbc_driver
+    extra_classpath new_resource.extra_classpath
+    jdbc_properties new_resource.jdbc_properties
+    command "USE [#{new_resource.database}]; #{update_user_sql}"
+    not_if_sql <<-SQL
+      SELECT
+        U.name AS [user],
+        SP.name AS [login]
+      FROM
+        sys.database_principals U
+      JOIN sys.server_principals SP ON SP.sid = U.sid AND SP.is_disabled = 0 AND SP.name NOT LIKE 'NT AUTHORITY\\%' AND SP.name NOT LIKE 'NT SERVICE\\%'
+      WHERE
+        U.name != 'dbo' AND
+        U.type_desc IN ('SQL_USER','WINDOWS_USER','WINDOWS_GROUP') AND
+        U.name = '#{new_resource.user}' AND
+        SP.name = '#{new_resource.login}'
+    SQL
+  end
 end
 
 action :drop do
